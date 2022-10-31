8c8c745de97b38515bcb1bcccf31a5c1de4d6c7d
BigJeffTheChef <marleygpeter@gmail.com>
2022-10-31 13:16:43 +0000

    iteration on booking trans

--- SQL/Transactions/InsertBooking.sql
+++ SQL/Transactions/InsertBooking.sql
@@ -91,8 +91,9 @@ START TRANSACTION;
     SET @BookingContactId = LAST_INSERT_ID();
     
    	/********************************
-    -- BOOKING AND ITS LINE ITEMS
+    -- BOOKING
     ********************************/
+    
     SET @HolidayNightsDuration = 1;
     
     -- insert booking
@@ -108,9 +109,64 @@ START TRANSACTION;
     INSERT INTO booking_line_item (booking_line_item.booking_line_item_price_gbp, booking_line_item.booking_line_item_type_id, booking_line_item.booking_line_item_booking_id)
     VALUES (NULL, @HotelId, @LineItemHotelId, @BookingId);
     
-    -- insert line items
+    SELECT @HotelId := hotel.hotel_id, hotel.hotel_name FROM hotel WHERE hotel.hotel_name = 'Spanish Hotel 1';
+
+    -- get room type date/time based price
+
+    SELECT @HotelRoomType1Id := room_type.room_type_id FROM room_type WHERE room_type.room_type_hotel_id = @HotelId AND room_type.room_type_name = 'Double or Twin room';
+    SELECT @HotelRoomType2Id := room_type.room_type_id FROM room_type WHERE room_type.room_type_hotel_id = @HotelId AND room_type.room_type_name = 'Double or Twin room - Sleeps up to 3';
+    SELECT @HotelRoomType3Id := room_type.room_type_id FROM room_type WHERE room_type.room_type_hotel_id = @HotelId AND room_type.room_type_name = 'Double or Twin room with Pool room';
+
+    SELECT @HotelRoomType1Cost := room_type_price.room_type_price_gbp FROM room_type_price WHERE DATE(@TargetDateTime) BETWEEN room_type_price.room_type_price_valid_from__date AND room_type_price.room_type_price_valid_to_date AND room_type_price.room_type_id = @HotelRoomType1Id; -- passengers 1 and 2 to share
+
+    SELECT @HotelRoomType2Cost := room_type_price.room_type_price_gbp FROM room_type_price WHERE DATE(@TargetDateTime) BETWEEN room_type_price.room_type_price_valid_from__date AND room_type_price.room_type_price_valid_to_date AND room_type_price.room_type_id = @HotelRoomType2Id; -- passenger 3
+
+    -- get board cost
+
+    SELECT @BoardType1Id := board_type.board_type_id FROM board_type WHERE board_type.board_type_name = 'All Inclusive';
+    SELECT @BoardType2Id := board_type.board_type_id FROM board_type WHERE board_type.board_type_name = 'Bed And Breakfast';
+
+    SELECT @HotelBoardType1Id := hotel_board_type.hotel_board_type_id, @HotelBoardType1Cost := hotel_board_type.hotel_board_fee_gbp FROM hotel_board_type WHERE hotel_board_type.hotel_id = @HotelId AND hotel_board_type.board_type_id = @BoardType1Id;
+    SELECT @HotelBoardType2Id := hotel_board_type.hotel_board_type_id, @HotelBoardType2Cost := hotel_board_type.hotel_board_fee_gbp FROM hotel_board_type WHERE hotel_board_type.hotel_id = @HotelId AND hotel_board_type.board_type_id = @BoardType2Id;
+
+    -- insert room_bookings
+    INSERT INTO room_booking (room_booking.room_booking_room_type_id, room_booking.room_booking_booking_id)
+    VALUES (@HotelRoomType1Id, @BookingId);
+
+    INSERT INTO room_booking (room_booking.room_booking_room_type_id, room_booking.room_booking_booking_id)
+    VALUES (@HotelRoomType2Id, @BookingId);
+	
+    /********************************
+    -- BOOKING LINE ITEMS
+    ********************************/
+    -- hotel rooms (2 for this booking)
+    INSERT INTO booking_line_item (booking_line_item.booking_line_item_price_gbp, booking_line_item.booking_line_item_type_id, booking_line_item.booking_line_item_booking_id)
+    VALUES (@HotelRoomType1Cost, @LineItemHotelId, @HotelRoomType1Cost, @BookingId);
+    
+    INSERT INTO booking_line_item (booking_line_item.booking_line_item_price_gbp, booking_line_item.booking_line_item_type_id, booking_line_item.booking_line_item_booking_id)
+    VALUES (@HotelRoomType2Cost, @LineItemHotelId, @HotelRoomType2Cost, @BookingId);
+    
+    -- flights, outbound and return for each passenger
+    INSERT INTO booking_line_item (booking_line_item.booking_line_item_price_gbp, booking_line_item.booking_line_item_type_id, booking_line_item.booking_line_item_booking_id)
+    VALUES 
+        (@HotelRoomType1Cost,@LineItemHotelId,  @BookingId), -- hotel room 1 (including board cost)
+        (@HotelRoomType2Cost, @LineItemHotelId, @BookingId), -- hotel room 2 (including board cost)
+        (@OutboundFlightPrice, @LineItemOutboundFlightId, @BookingId), -- passenger 1 outbound flight
+        (@OutboundFlightPrice, @LineItemOutboundFlightId, @BookingId), -- passenger 2 outbound flight
+        (@OutboundFlightPrice, @LineItemOutboundFlightId, @BookingId), -- passenger 3 outbound flight
+        (@OutboundFlightPrice, @LineItemReturnFlightId, @BookingId), -- passenger 1 return flight
+        (@OutboundFlightPrice, @LineItemReturnFlightId, @BookingId), -- passenger 2 return flight
+        (@OutboundFlightPrice, @LineItemReturnFlightId, @BookingId); -- passenger 3 return flight
+
+	/********************************
+    -- UPDATE BOOKING TOTAL PRICE
+    ********************************/
+    
+    UPDATE 
+    -- insert line items DONE
     -- insert booking contact DONE
-    -- flights DONE
-	-- accomodation
-	-- insert booking
+    -- flights (stored via booking) DONE
+	-- accomodation (stored via room_booking) DONE
+	-- insert booking DONE
+    -- update booking cost
 COMMIT;
